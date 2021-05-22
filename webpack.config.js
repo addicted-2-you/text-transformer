59944756d2672993b46f84d8bc344b1bfb6e69fd
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2021-05-23 01:30:47 +0300

    init config

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/index.jsx'],
+  target: 'web',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx'],
+    alias: {
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
+      constants: path.resolve(__dirname, './src/constants'),
+      models: path.resolve(__dirname, './src/models'),
+      store: path.resolve(__dirname, './src/store'),
+      utils: path.resolve(__dirname, './src/utils'),
+    },
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        use: 'babel-loader',
+        exclude: [/node_modules/],
+      },
+      { test: /\.(sc|sa|c)ss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+    new MiniCssExtractPlugin({ filename: 'index.css' }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
