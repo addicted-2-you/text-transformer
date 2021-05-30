147830d3f69b0d4c912f185506daa0a6e6bd6ce4
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2021-05-30 18:57:42 +0300

    add copying output text

--- src/widgets/output-area/output-area.jsx
+++ src/widgets/output-area/output-area.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function OutputArea() {
+  const outputTextareaRef = React.useRef(null);
+
   const outputText = useSelector((state) => state.text.output);
+
+  function copyOutputText() {
+    window.navigator.clipboard.writeText(outputText);
+  }
 
   return (
     <div className="output-area">
-      <pre className="output-text">{outputText}</pre>
+      <textarea ref={outputTextareaRef} cols="30" rows="10" value={outputText} disabled />
+      <button className="output-area__copy-button" type="button" onClick={copyOutputText}>
+        Copy
+      </button>
     </div>
   );
 }
