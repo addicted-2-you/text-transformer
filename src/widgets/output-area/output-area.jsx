2aaaca94aadf2be044c509e90b662cc4349f5e7e
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2021-05-30 19:39:40 +0300

    add feedback on output text copy

--- src/widgets/output-area/output-area.jsx
+++ src/widgets/output-area/output-area.jsx
@@ -4,17 +4,23 @@ import { useSelector } from 'react-redux';
 function OutputArea() {
   const outputTextareaRef = React.useRef(null);
 
+  const [copyOutputTextButtonText, setCopyOutputTextButtonText] = React.useState('Copy');
+
   const outputText = useSelector((state) => state.text.output);
 
-  function copyOutputText() {
+  function onCopyOutputTextClick() {
     window.navigator.clipboard.writeText(outputText);
+    setCopyOutputTextButtonText('Copied!');
+    setTimeout(() => {
+      setCopyOutputTextButtonText('Copy');
+    }, 1000);
   }
 
   return (
     <div className="output-area">
       <textarea ref={outputTextareaRef} cols="30" rows="10" value={outputText} disabled />
-      <button className="output-area__copy-button" type="button" onClick={copyOutputText}>
-        Copy
+      <button className="output-area__copy-button" type="button" onClick={onCopyOutputTextClick}>
+        {copyOutputTextButtonText}
       </button>
     </div>
   );
